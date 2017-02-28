8f2b1c14cfdfd1f59528fa6255a62feb012b7968
ArnoldPires <arnold.pires92@gmail.com>
2017-02-28 00:26:25 -0500

    first

---
+++ main.js
@@ -0,0 +1,36 @@
+wvar viewer = new Cesium.Viewer('cesiumContainer', {
+    infoBox : false,
+    selectionIndicator : false,
+    shadows : true
+});
+
+function createModel(url, height) {
+    viewer.entities.removeAll();
+
+    var position = Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, height);
+    var heading = Cesium.Math.toRadians(135);
+    var pitch = 0;
+    var roll = 0;
+    var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+    var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
+    var entity = viewer.entities.add({
+        name : url,
+        position : position,
+        orientation : orientation,
+        model : {
+            uri : url,
+            minimumPixelSize : 128,
+            maximumScale : 20000
+        }
+    });
+    viewer.trackedEntity = entity;
+}
+
+
+{
+    text : 'Skinned character',
+    onselect : function() {
+        createModel('../../SampleData/models/CesiumMan/Cesium_Man.glb', 0);
+    }
+}];
